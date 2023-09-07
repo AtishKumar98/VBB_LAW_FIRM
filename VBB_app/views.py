7c202424c9b19767e9fc757702844859518ae0d6
AtishKumar98 <atishkumar31518@gmail.com>
2023-09-07 15:51:52 +0530

    commitFor_profile

--- VBB_app/views.py
+++ VBB_app/views.py
@@ -159,23 +159,14 @@ profiles = {
         'role': 'Lawyer Core-Team',
         'image':'/../static/images/BD_Tripathi-removebg-preview_by@2x.png',
         'description':"""
-    <h4>The Legal Guardian: Navigating the Complex World of Law</h4>
+    <h4>Dhwani S. Rughani, a dynamic advocate at VBB Legal, shines as a promising newcomer in the legal arena.</h4>
 
-    <p>Lawyers, often referred to as the architects of justice, are the unsung heroes of our society. They are the individuals who provide a voice to those in need, defending their rights, and ensuring that justice prevails in the face of adversity. Lawyers play a pivotal role in upholding the principles of law and order in our increasingly complex world.</p>
+    <p>Dhwani's dedication to justice is evident through her areas of expertise:<br/>
 
-    <h5>A Noble Profession:</h2>
-
-    <p>The legal profession is one of the oldest and most respected callings in human history. Lawyers have existed in various forms in nearly all civilizations, dating back to ancient Greece, Rome, and beyond. The role of lawyers has evolved over time, but their fundamental purpose has remained consistent: to advocate for their clients' rights and uphold the rule of law.</p>
-
-    <h5>Education and Training:</h2>
-
-    <p>Becoming a lawyer is not an easy path. It requires a rigorous educational journey that typically begins with a bachelor's degree in a related field. Most aspiring lawyers then pursue a Juris Doctor (JD) degree from an accredited law school. This graduate-level program is designed to equip students with the legal knowledge and skills necessary to practice law.</p>
-
-    <!-- Add more content as needed -->
-
-    <h5>Conclusion:</h2>
-
-    <p>In a world where legal complexities are ever-present, lawyers stand as the guardians of justice, ensuring that the principles of fairness and equity prevail. They are educators, advisors, advocates, and problem solvers, dedicated to upholding the rule of law and protecting the rights of individuals and organizations alike. With unwavering commitment to ethical principles and an unyielding pursuit of justice, lawyers are an essential pillar of a just and orderly society, making the world a better place, one case at a time.</p>
+Intellectual Property Law: Skillfully navigating the complexities of patents, trademarks, and copyrights.<br/>
+Medical Negligence Cases: Fearlessly advocating for clients affected by medical malpractice, ensuring their voices are heard.<br/>
+Cases Against Government: Tenaciously safeguarding individual rights in legal battles against state and central government entities.<br/>
+As a fresh advocate, Dhwani's impact is felt at VBB Legal, where her commitment to ethics and diligence align seamlessly with the firm's values. With each case she handles, Dhwani demonstrates an unwavering commitment to justice and a promising future in the legal profession.</p>
 """ ,
     },
 }
